290a3d9f854057378009dd371601d18f3f9ac5af
CristiTr <cristian.tranulea@gmail.com>
2015-09-26 15:23:49 +0300

    with deffered

--- _app/js/food.js
+++ _app/js/food.js
@@ -2,6 +2,7 @@
 var Food = {};
 
 Food.getRecipes = function(ingredients) {
+    var deferred = $.Deferred();
     $.ajax({
         url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients",
         type: "GET",
@@ -13,29 +14,34 @@ Food.getRecipes = function(ingredients) {
         }
     }).then(function(data) {
         console.log(data);
+        Food.getInsta(data[0].title);
+        deferred.resolve(data);
     });
+    return deferred.promise();
 }
 Food.getRecipes("milk, eggs");
 
 Food.getInsta = function(tag) {
+    tag = tag.replace(" ", "");
+    var deferred = $.Deferred();
     $.ajax({
         url: "https://api.instagram.com/v1/tags/" + tag + "/media/recent",
-        dataType:"jsonp",                
+        dataType: "jsonp",
         data: {
             client_id: "e09cbeff335c4b4aa27071d684d0b840"
-        },  
-        type:"GET",
-        headers:{"Access-Control-Allow-Origin":"*"
-   }       
+        },
+        type: "GET",
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
     }).then(function(data) {
 
-    	var imgArray=[];
-    	for(var i=0;i<data.data.length;i++)
-    	{
-    		imgArray.push(data.data[i].images.standard_resolution.url);
-    	}
+        var imgArray = [];
+        for (var i = 0; i < data.data.length; i++) {
+            imgArray.push(data.data[i].images.standard_resolution.url);
+        }
         console.log(imgArray);
+        deferred.resolve(data);
     });
+    return deferred.promise();
 }
-
-
