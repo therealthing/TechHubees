742542fec1fa09952b7979820bccfd8962a3918a
CristiTr <cristian.tranulea@gmail.com>
2015-09-26 18:38:34 +0300

    asdfa

--- _app/js/food.js
+++ _app/js/food.js
@@ -56,6 +56,9 @@ Food.getIngredients = function(ingredients) {
 
 Food.getInsta = function(tag) {
     tag = tag.replace(" ", "");
+    tag = tag.replace("'","");
+    tag = tag.replace('"',"");
+    
     var deferred = $.Deferred();
     $.ajax({
         url: "https://api.instagram.com/v1/tags/" + tag + "/media/recent",
