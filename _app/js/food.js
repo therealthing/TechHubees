6dbd63c0cfdd82c9873edc88975722865b51c509
CristiTr <cristian.tranulea@gmail.com>
2015-09-26 14:50:25 +0300

    ini

---
+++ _app/js/food.js
@@ -0,0 +1,41 @@
+//food nutrition
+var Food = {};
+
+Food.getRecipes = function(ingredients) {
+    $.ajax({
+        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients",
+        type: "GET",
+        data: {
+            ingredients: ingredients
+        },
+        headers: {
+            "X-Mashape-Key": "jis02SUQJVmsh25L1vfXvYhJAvxyp1AaDFAjsndi1cy5fhnK8H"
+        }
+    }).then(function(data) {
+        console.log(data);
+    });
+}
+Food.getRecipes("milk, eggs");
+
+Food.getInsta = function(tag) {
+    $.ajax({
+        url: "https://api.instagram.com/v1/tags/" + tag + "/media/recent",
+        dataType:"jsonp",                
+        data: {
+            client_id: "e09cbeff335c4b4aa27071d684d0b840"
+        },  
+        type:"GET",
+        headers:{"Access-Control-Allow-Origin":"*"
+   }       
+    }).then(function(data) {
+
+    	var imgArray=[];
+    	for(var i=0;i<data.data.length;i++)
+    	{
+    		imgArray.push(data.data[i].images.standard_resolution.url);
+    	}
+        console.log(imgArray);
+    });
+}
+
+
